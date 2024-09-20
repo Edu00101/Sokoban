cc59f03bbdf6b28a008c7255762498938e3dc261
commit <eduardoiury17@gmail.com>
2024-09-20 15:13:54 -0300

    Fiz o bola andar!

--- projeto.js
+++ projeto.js
@@ -1,30 +1,34 @@
-
-
-const playerElemnt = document.querySelector('.player');
-console.log(playerElemnt);
-
-playerElemnt.addEventListener("click", function () {
-    alert("clicou no jogador");
-})
-window.addEventListener("keydown", function (event) {
-    nextPositioin(event.code);
- 
-})
-const cell = document.querySelector('.cell');
-console.log(cell);
-
-
-function nextPositioin(keycode) {
-    if (keycode == 'ArrowUp') player.X--;
-    if (keycode == 'ArrowDown') player.X++;
-    if (keycode == 'ArrowRight')player.Y--;
-        if (keycode == 'ArrowLeft')player.Y++;
-    
-    console.log(keycode, player);
-}
 function Player(X, Y) {
     this.x = X;
     this.y = Y;
 }
-
 const player = new Player(0, 0);
+
+
+const playerElement = document.querySelector('.player');
+console.log(playerElement);
+
+const celulas = document.querySelectorAll('.cell');
+console.log(celulas);
+
+
+window.addEventListener("keydown", function (event) {
+    
+    console.log(event.code);
+   
+    nextPositioin(event.code);
+})
+
+function nextPositioin(keycode) {
+    if (keycode == 'ArrowUp') player.x--;
+    if (keycode == 'ArrowDown') player.x++;
+    if (keycode == 'ArrowRight') player.y++;
+    if (keycode == 'ArrowLeft') player.y--;
+
+    console.log(keycode, player);
+
+    const K = player.x * 4 + player.y;
+
+    celulas[K].append(playerElement);
+}
+
