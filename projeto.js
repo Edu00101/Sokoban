73998fa6128c624ded6b7bcb46e10801d0274cf5
commit <eduardoiury17@gmail.com>
2024-09-24 13:54:48 -0300

    Modificação de espaço

--- projeto.js
+++ projeto.js
@@ -2,11 +2,17 @@ const player = new Player(0, 0);
 const celulas = document.querySelectorAll('.cell');
 const playerElement = document.querySelector('.player');
 
+const DIST_SALTO = 66;
+const MARGIN_FIX = 4;
+
+playerElement.style.top = calculaPosicao(0);
+playerElement.style.left = calculaPosicao(0);
+
 window.addEventListener("keydown", function (event) {
     const next = player.nextPosition(event.code);
 
     if (verifyPosition(next)) {
-        let k = next.x * 4 + next.y;
+        let k = next.x * 8 + next.y;
         player.moveTo(next, playerElement, celulas[k]);
     }
 })
@@ -32,14 +38,26 @@ function Player(x, y) {
         this.x = position.x;
         this.y = position.y;
 
-        parent.append(element);
+element.style.top = calculaPosicao(this.x);
+element.style.left = calculaPosicao(this.y);
     }
 }
 function verifyPosition(position) {
     let { x, y } = position
 
-    return x >= 0 && x < 4 && y >= 0 && y < 4
+    return x >= 0 && x < 8 && y >= 0 && y < 8
 
 
 }
+
+function calculaPosicao(qtd, tamanho) {
+
+    return (qtd * DIST_SALTO + MARGIN_FIX  + "px");
+}
+
+console.log(calculaPosicao(0, 64) === "0px");
+console.log(calculaPosicao(1, 64) === "64px");
+console.log(calculaPosicao(2, 32) === "64px");
+console.log(calculaPosicao(10, 60) === "600px");
+console.log(calculaPosicao(-3, 45) === "-135px");
 
