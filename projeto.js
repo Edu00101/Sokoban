7a52d6c5d7eaad4cf9161ca13f5a94afabfbf1d4
commit <eduardoiury17@gmail.com>
2024-09-26 13:54:06 -0300

    Paredes colocadas, para andar no mapa

--- projeto.js
+++ projeto.js
@@ -1,17 +1,30 @@
+const boardMap = [
+    ["#", "#", "#", "#", "#", "#", "#", "#"],
+    ["#", ".", ".", ".", ".", ".", ".", "#"],
+    ["#", ".", ".", ".", "#", ".", ".", "#"],
+    ["#", ".", "#", "G", ".", ".", ".", "#"],
+    ["#", ".", ".", "G", "B", "#", ".", "#"],
+    ["#", ".", ".", "#", ".", "B", ".", "#"],
+    ["#", ".", ".", "P", ".", ".", ".", "#"],
+    ["#", "#", "#", "#", "#", "#", "#", "#"]
+
+]
+
+const NUM_ROWS = boardMap.length;
+const NUM_COLS = boardMap[0].length;
+
 const DIST_SALTO = 66;
 const MARGIN_FIX = 4;
-const NUM_ROWS = 8;
-const NUM_COLS = 8;
 
 buildGameBoard(NUM_ROWS, NUM_COLS);
 
 
-const player = new Player(0, 0);
+const player = new Player(1, 1);
 const playerElement = document.querySelector('.player');
 
 
-playerElement.style.top = calculaPosicao(0);
-playerElement.style.left = calculaPosicao(0);
+playerElement.style.top = calculaPosicao(player.x);
+playerElement.style.left = calculaPosicao(player.y);
 
 window.addEventListener("keydown", function (event) {
     const next = player.nextPosition(event.code);
@@ -49,7 +62,7 @@ function Player(x, y) {
 function verifyPosition(position) {
     let { x, y } = position
 
-    return x >= 0 && x < NUM_ROWS && y >= 0 && y < NUM_COLS;
+    return boardMap[x][y] !== '#';
 
 
 }
@@ -58,7 +71,15 @@ function calculaPosicao(qtd, tamanho) {
 
     return (qtd * DIST_SALTO + MARGIN_FIX + "px");
 }
-function createGameElement(elementName, className, parentNode){
+function createGameElement(elementName, className, parentNode) {
+    const element = document.createElement(elementName);
+    element.classList.add(className);
+    parentNode.append(element);
+
+    return element;
+}
+
+function createGameElement(elementName, className, parentNode) {
     const element = document.createElement(elementName);
     element.classList.add(className);
     parentNode.append(element);
@@ -69,18 +90,23 @@ function createGameElement(elementName, className, parentNode){
 function buildGameBoard(numberOfRows, numberOfCollumns, rule) {
     const game = document.getElementById("jogo");
     const board = createGameElement('div', 'bloco', game);
- 
 
-    for (let X = 0; X < numberOfRows; X++) {
+
+    for (let x = 0; x < numberOfRows; x++) {
         const row = createGameElement('div', 'row', board);
 
-        for (let Y = 0; Y < numberOfCollumns; Y++) {
-        createGameElement('div','cell', row);
-            
-            };
+        for (let y = 0; y < numberOfCollumns; y++) {
+            const cell = createGameElement('div', 'cell', row);
+            const bir = boardMap[x][y];
+
+
+            if (bir === '#') cell.classList.add('wall');
+            if (bir === 'B') cell.classList.add('goal');
+            if (bir === 'G') cell.classList.add('box');
+
 
         }
-        createGameElement('div', 'player', board);
 
     }
-   
+    createGameElement('div', 'player', board);
+}
