fddc6406618cfc1f2e9259c860c344fed1ac3ba1
commit <eduardoiury17@gmail.com>
2024-09-27 15:11:34 -0300

    Tranfere "Pieces" para o arquivo pieces.js

--- Sokoban/projeto.js
+++ Sokoban/projeto.js
@@ -24,30 +24,6 @@ window.addEventListener("keydown", function (event) {
 })
 
 
-function Pieces(x, y) {
-    this.x = x;
-    this.y = y;
-
-    this.nextPosition = function (keycode) {
-        let { x, y } = this;
-
-        if (keycode == 'ArrowUp') x--;
-        if (keycode == 'ArrowDown') x++;
-        if (keycode == 'ArrowRight') y++;
-        if (keycode == 'ArrowLeft') y--;
-
-        return { x, y };
-
-    }
-    this.moveTo = function (position, element) {
-
-        this.x = position.x;
-        this.y = position.y;
-
-        element.style.top = calculaPosicao(this.x);
-        element.style.left = calculaPosicao(this.y);
-    }
-}
 function verifyPosition(position) {
     let { x, y } = position
 
