7a6a976f33fbabe5cf6ed9a1283f0bb5a8e8fe0f
commit <eduardoiury17@gmail.com>
2024-09-27 14:54:21 -0300

    Tranfere "boardMap", para o arquivo board.js

--- Sokoban/projeto.js
+++ Sokoban/projeto.js
@@ -87,28 +87,3 @@ function createGameElement(elementName, className, parentNode) {
     return element;
 }
 
-function buildGameBoard(numberOfRows, numberOfCollumns) {
-    const game = document.getElementById("jogo");
-    const board = createGameElement('div', 'bloco', game);
-    const pices = {};
-
-
-    for (let x = 0; x < numberOfRows; x++) {
-        const row = createGameElement('div', 'row', board);
-
-        for (let y = 0; y < numberOfCollumns; y++) {
-            const cell = createGameElement('div', 'cell', row);
-            const bir = boardMap[x][y];
-
-
-            if (bir === '#') cell.classList.add('wall');
-            if (bir === 'G') cell.classList.add('goal');
-            if (bir === 'B') cell.classList.add('box');
-            if (bir === 'P') pices.player = { x: x, y: y }
-        }
-
-
-    }
-
-    return pices;
-}
