5ac76ba43ab43a3fe93019a664fb195a8d0bff60
commit <eduardoiury17@gmail.com>
2024-09-27 15:58:17 -0300

    Criar função do "playerElement" createBoardPiece()

--- Sokoban/projeto.js
+++ Sokoban/projeto.js
@@ -3,25 +3,31 @@ const NUM_ROWS = boardMap.length;
 const NUM_COLS = boardMap[0].length;
 const DIST_SALTO = 66;
 const MARGIN_FIX = 4;
+
 const pieces = buildGameBoard(NUM_ROWS, NUM_COLS);
+const board = document.querySelector('.bloco');
 
 
 const player = new Pieces(pieces.player.x, pieces.player.y);
-const board = document.querySelector('.bloco');
-const playerElement = createGameElement('div', 'caixa', board);
+const element = createBoardPiece(player, 'player');
 
-
-
-playerElement.style.top = calculaPosicao(player.x);
-playerElement.style.left = calculaPosicao(player.y);
 
 window.addEventListener("keydown", function (event) {
     const next = player.nextPosition(event.code);
 
     if (verifyPosition(next)) {
-        player.moveTo(next, playerElement);
+        player.moveTo(next, element);
     }
 })
+
+function createBoardPiece(piece, className) {
+    const element = createGameElement('div', className, board);
+
+    element.style.top = calculaPosicao(piece.x);
+    element.style.left = calculaPosicao(piece.y);
+
+    return element;
+}
 
 
 function verifyPosition(position) {
