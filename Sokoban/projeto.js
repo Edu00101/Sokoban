5323cbe6314d47075aae0299f02524fa51162e37
commit <eduardoiury17@gmail.com>
2024-10-01 16:08:41 -0300

    Usando array find()

--- Sokoban/projeto.js
+++ Sokoban/projeto.js
@@ -24,9 +24,25 @@ for (let x = 0; x < pieces.boxes.length; x++) {
         boxes.push(piece);
 }
 
-        createBoardPiece(pieces.boxes[x], 'caixas');
+
+window.addEventListener("keydown", function (event) {
+    // event.preventDefault();
+
+    handlePieceMovement(event.code);
+});
+
+/** Tarefa #1: implementar função para localizar uma caixa à partir de um
+ * uma dada coordenada.
+*/
+function findBoxAtPosition(position) {
+    // modificar linha(s) de código abaixo
+    return boxes.find((caixa) => caixa.x === position.x && position.y === caixa.y);
+    
 }
 
+console.log(findBoxAtPosition({x:5, y:5}));
+console.log(findBoxAtPosition({x:3, y:3}));
+console.log(findBoxAtPosition({x:4, y:4}));
 
 window.addEventListener("keydown", function (event) {
     const next = playerPiece.nextPosition(event.code);
